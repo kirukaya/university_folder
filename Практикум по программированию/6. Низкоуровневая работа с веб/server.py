9d5559c10237c4872e67eb26f95c0fd3581090b9
Gennady Kuznetsov <only1exit@gmail.com>
2021-12-17 02:53:44 +0300

    done

--- Практикум по программированию/6. Низкоуровневая работа с веб/server.py
+++ Практикум по программированию/6. Низкоуровневая работа с веб/server.py
@@ -1,41 +1,92 @@
-import socket
 from datetime import datetime
+import socket
+import threading
+import random
+import json
+import os
+import re
+import magic
 
-sock = socket.socket()
+def getConfig(filename = 'config.json'):
+    with open(filename, 'r') as file:
+        config = json.load(file)
+    return config['port'], config['request_volume'], config['root']
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+def bindSocket(port):
+    sock = socket.socket()
+    while True:
+        try: 
+            sock.bind(('', port))
+            print(f'Using port {port}')
+            return sock
+        except:
+            port = random.randint(1024, 65535)
 
-sock.listen(5)
-
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg, end = '')
-
-
-content = ''
-with open('index.html', 'r', encoding='utf-8') as f:
-    for line in f:
-        content += line
-content_length = len(content)
-
-resp = f"""HTTP/1.1 200 OK
-Date: {datetime.now()}
+def http_response(content, request_date, addr, resource, http_response_code, content_type):
+    content_length = len(content)
+    log = [str(item) for item in [request_date, addr, resource, http_response_code]]
+    log = "; ".join(log)
+    with LOCK:
+        with open(logfile, "a", encoding = "utf8") as logfl:
+            logfl.write(log+"\n")
+    response = f"""HTTP/1.1 {http_response_code}
+Date: {str(datetime.now())[-6]}
 Content-length: {content_length}
 Server: SelfMadeServer v0.0.1
-Content-type: text/html, charset = utf8
-Connection: close
+Content-type: {content_type}
+Connection: retry-after
 
-{content}"""
+"""
+    response = response.encode() + content
+    return response
 
-conn.send(resp.encode())
+def resource_parser(request):
+    request = request.split("\r\n")
+    http_request = request[0].split()[1]
+    request_path = re.split("[\\/]", http_request)
+    if request_path == ["", ""]:
+        request_path = ["index.html"]
+    allowed = bool(re.search("\.(html|css|js|png|jpg|jpeg|pdf)$", request_path[-1]))
+    return request_path, http_request, allowed
 
-conn.close()
+def request_reader(request):
+    resource, http_path, allowed = resource_parser(request)
+    resource = os.path.join(root, *resource)
+    content_type = "text/html"
+    if allowed:
+        
+        try:
+            with open(resource, "rb") as contentfile:
+                content = contentfile.read(1024*1024*16)
+            response_code = "200 OK"
+            mime = magic.Magic(mime=True)
+            content_type = mime.from_file(resource)
+        except:
+            content = b""
+            response_code = "404 Not Found"
+    else:
+        content = b""
+        response_code = "403 Forbidden"
+    return content, http_path, response_code, content_type
+
+def clientThread(conn, addr):
+    while True:
+        request = conn.recv(request_volume).decode()
+        request_date = str(datetime.now())[:-6]
+        if not request:
+            continue
+        content, resource, response_code, content_type = request_reader(request)
+        conn.send(http_response(content, request_date, addr, resource, response_code,content_type)) 
+
+if __name__ == '__main__':
+    LOCK = threading.Lock()
+    logfile = 'log.txt'
+    port, request_volume, root = getConfig()
+    sock = bindSocket(port)
+    sock.listen(5)
+    while True:
+        conn, addr = sock.accept()
+        with LOCK: 
+            print(f'User {addr} connected.')
+        threading.Thread(target = clientThread, args = (conn, addr), daemon = True).start()
+    conn.close()
